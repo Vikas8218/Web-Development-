25002235aba45ecf636d3212a8b26ffb591040ee
Vikas Saini <vikas08218@gmail.com>
2024-07-28 10:32:01 +0530

    submit

---
+++ video--54/script.js
@@ -0,0 +1,15 @@
+
+    alert("hello world !!");
+
+  console.log("code is running")
+   var a =prompt("Enter your number")
+   var isTrue =confirm("Are you sure you want to leave this page and blast your computer")
+   if(isTrue){
+    console.log("Computer is blasting")
+   }
+   else{
+    console.log("Computer is not blasting")
+   }
+
+   console.log("Your number is"+a)
+   
